360930720304f259f20b9755442c9ad7737cc109
Christian <cjimenezcdam1@gmail.com>
2020-02-20 23:14:21 +0100

    SE COMPLETA LA CLASE CURSO - Finalización del método getFechaFormateada() - Se realizan pruebas con el main() y los resultados son exitosos - Se da la clase por finalizada

--- Curso.java
+++ Curso.java
@@ -75,7 +75,8 @@ public class Curso
      *  de la forma "día nombre-mes año"  Ej. "03 diciembre 2019" 
      */
     public String getFechaFormateada() {
-        return "";
+        LocalDate hoy = fecha;
+        return hoy.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
     }
 
     /**
